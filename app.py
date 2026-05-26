fa844fc3b0bd3cadd91ed75eea5744105e57f05f
maribelcanoflores06-lgtm <maribel.cano.flores06@gmail.com>
2026-05-25 23:05:49 -0500

    Reformat app.py with consistent indentation

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if sesion=="Sesion 1":
   st.write("Bienvenidos la sesion 1")
 st.image("COLORES.png")
 
-elif sesion=="Sesion 2":
+  elif sesion=="Sesion 2":
   st.write("Bienvenidos a la sesion 2")
 elif sesion=="Sesion 3":
   st.write("Bienvenido a la sesion 3 ")
