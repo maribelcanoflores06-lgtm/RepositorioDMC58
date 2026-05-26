6f55fd59707d414a12d86dadbb45fd935ef9a76d
maribelcanoflores06-lgtm <maribel.cano.flores06@gmail.com>
2026-05-25 22:53:07 -0500

    Add session selection to Streamlit app

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ st.title("Mi primera aplicación en python")
 st.sidebar.title("Parámetros")
 
 st.write("elaborado por Maribel Cano")
+
+sesion=st.selectbox("Seleccione una sesión",["Sesion 1","Sesion 2","sesion 3],"Sesion 4")
+
